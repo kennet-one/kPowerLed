d7f9b43198fc80cafd9fdcc78706603bc596e09d
kennet-one <kennet1one@gmail.com>
2025-03-08 01:06:20 +0100

    bug fix

--- kPowerLed.ino
+++ kPowerLed.ino
@@ -10,36 +10,21 @@ Scheduler userScheduler; // to control your personal task
 painlessMesh  mesh;
 
 
-char buttonState = 1;
+char buttonState = 0;
 
 void receivedCallback( uint32_t from, String &msg ) {
 
   String str1 = msg.c_str();
-  String str2 = "powled";
-  //String str3 = "garland_echo";
+  String str2 = "powled0";
+  String str3 = "powled1";
 
   if (str1.equals(str2)) {
-
-    if (buttonState == 1) {
-      buttonState = 0;
-      //mesh.sendSingle(2661345693,"garland_off");
-      //mesh.sendSingle(624409705,"garland_off");
-    } else {
-      buttonState++;
-      //mesh.sendSingle(2661345693,"garland_on");
-      //mesh.sendSingle(624409705,"garland_on");
-    } 
+    buttonState = 0;
   }
 
-  // if (str1.equals(str3)) {
-  //   if (buttonState == 0) {
-  //     mesh.sendSingle(2661345693,"garland_off");
-  //     mesh.sendSingle(624409705,"garland_off");
-  //   } else {
-  //     mesh.sendSingle(2661345693,"garland_on");
-  //     mesh.sendSingle(624409705,"garland_on");
-  //   }
-  // }
+  if (str1.equals(str3)) {
+    buttonState = 1;
+  }
 }
 
 
