f4ade9306b4acb5f39f7c9a9a281d2bf0c639d45
kennet-one <kennet1one@gmail.com>
2025-11-11 00:15:42 +0100

    critical bug fix

--- kPowerLed.ino
+++ kPowerLed.ino
@@ -4,43 +4,38 @@
 //************************************************************
 #include "painlessMesh.h"
 #include "mash_parameter.h"
-#include "CRCMASH.h"
-
+#include "CRC.h"
 
 Scheduler userScheduler; // to control your personal task
-// (disabled) painlessMesh mesh; // now provided by CRCMASH.h
+painlessMesh mesh; 
+
 char buttonState = 0;
 
-void handleBodyFrom( uint32_t from, const String &body ) {
+void handleBody( const String &msg ) {
 
-  String str1 = body.c_str();
-  String str2 = "powled0";
-  String str3 = "powled1";
-
-  if (str1.equals(str2)) {
+  if (msg.equals("powled0")) {
     buttonState = 0;
   }
 
-  if (str1.equals(str3)) {
+  if (msg.equals("powled1")) {
     buttonState = 1;
   }
 }
 
-
 void setup() {
   Serial.begin(115200);
+
+  WiFi.setSleep(false);
 
   pinMode(33, OUTPUT);
 
   mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
   mesh.onReceive(&receivedCallback);
-
 }
 
 void loop() {
 
-  // === CRCMASH Variant B: (from,body) queue ===
-  for (uint8_t _i=0; _i<4; ++_i){ uint32_t _from; String _b; if (!qPop2(_from, _b)) break; handleBodyFrom(_from, _b); }
+  for (uint8_t _i=0; _i<4; ++_i){ String _b; if (!qPop(_b)) break; handleBody(_b); }
   mesh.update();
 
   if (buttonState == 0) {
